7a7f81d828d5b26b67c4113d5aaf88ad00ac7d8f
Sourin <iamsourinsarkar@gmail.com>
2023-04-11 10:47:35 +0530

    Implemented Aspect Ratio

---
+++ src/components/ProfileQueue.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function ProfileQueue() {
+  return (
+    <>
+    <div>
+        <div class="flex" >
+            <h4>Contributors</h4>
+            <span>204</span>
+        </div>
+    </div>
+    </>
+  )
+}
