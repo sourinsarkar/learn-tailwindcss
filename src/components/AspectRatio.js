a5ca93bc39a7397924f055c8de090f37f65f706a
Sourin <iamsourinsarkar@gmail.com>
2023-04-26 21:51:54 +0530

    Deleted all React Files

--- src/components/AspectRatio.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function AspectRatio() {
-  return (
-    <div>
-        <iframe title="spotVideo" class="w-full aspect-video" src="https://dgyugonj9a9mu.cloudfront.net/WD_Launch2_936c2b843b_1_6edc830940.mp4"></iframe>
-    </div>
-  )
-}
